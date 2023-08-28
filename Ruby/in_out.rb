edaa30f977beab9d13e3898c6c1411dc1dbf8f1c
Timmarcelino <valentim.marcelino@live.com>
2023-08-28 14:49:32 -0300

    Update
    Restar aprendizado em RUBY

---
+++ Ruby/in_out.rb
@@ -0,0 +1,3 @@
+print 'Digite seu nome: '
+name = gets.chomp
+puts "Hello #{name}!"
---
+++ Ruby/tp_Dados.rb
@@ -0,0 +1,22 @@
+integer_number = -20
+float_number = -20.05
+ruby_free_course =  true
+happiness =  "Programming with ruby"
+bitcode_array = [ 0, 1, 2]
+onebit_symbol = :ruby_symbol
+onebit_hash = {course: 'ruby', language: 'pt-Br', locale: 'onebitcode.com'}
+
+puts integer_number.class
+puts  float_number.class
+puts ruby_free_course.class
+puts happiness.class
+puts bitcode_array.class
+puts onebit_symbol.class
+puts onebit_hash.class
+
+onebit_dynamic = 2
+puts onebit_dynamic.class
+onebit_dynamic = "String type"
+puts onebit_dynamic.class
+onebit_dynamic = :onebit_symbol
+puts onebit_dynamic.class
